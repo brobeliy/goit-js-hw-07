724cb3fa76a314d279bbe2a9b7768d1e52ddb466
Anton Mischenko <smacks0ul@gmail.com>
2022-12-25 15:18:17 +0200

    all done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// 1.Разметка
+const imgGal = document.querySelector(".gallery");
+const item = galleryItems
+  .map(
+    ({ preview, original, description }) => `
+<div class="gallery__item">
+  <a class="gallery__link" href=""${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+  )
+  .join("");
+
+imgGal.insertAdjacentHTML("beforeend", item);
+//Разметка
+
+//2.Делегирование
+imgGal.addEventListener("click", onClick);
+function onClick(e) {
+  e.preventDefault();
+  if (e.target.className === "gallery") {
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <div class="modal"> <img src="${e.target.dataset.source}"></div>
+`);
+  instance.show();
+
+  window.addEventListener("keydown", closeModal);
+  function closeModal(e) {
+    if (e.code == "Escape") {
+      instance.close();
+      window.removeEventListener("keydown", closeModal);
+    }
+   
+  }
+}
+
+// console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const imgGal = document.querySelector(".gallery");
+const item = galleryItems
+  .map(
+    ({ preview, original, description }) => `
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+  )
+  .join("");
+
+imgGal.insertAdjacentHTML("beforeend", item);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
